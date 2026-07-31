91134d89a1882510839ae738a79c745358c64b41
HafiyScriptZ <ugchunter123@gmail.com>
2026-07-31 20:05:49 +0800

    Add Hafiy Fake Purchase script to remote_scripts.lua

--- remote_scripts.lua
+++ remote_scripts.lua
@@ -3,4 +3,8 @@ return {
         Game = "Auto Farm & Rebirth",
         Code = [[loadstring(game:HttpGet("https://pastefy.app/jxKSvIV5/raw"))()]]
     },
+    {
+        Game = "Hafiy Fake Purchase",
+        Code = [[loadstring(game:HttpGet("https://pastebin.com/raw/FNHtRsEw"))()]]
+    },
 }
