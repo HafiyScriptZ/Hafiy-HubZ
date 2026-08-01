4bf6a789f6db84a22be41c6c3930d9f5b9982b92
HafiyScriptZ <ugchunter123@gmail.com>
2026-08-01 14:38:35 +0800

    Update remote_scripts.lua

--- remote_scripts.lua
+++ remote_scripts.lua
@@ -4,7 +4,11 @@ return {
         Code = [[loadstring(game:HttpGet("https://pastefy.app/2CzxhRAo/raw"))()]]
     },
     {
-        Game = "Hafiy Fake Purchase",
-        Code = [[loadstring(game:HttpGet("https://pastefy.app/OR9lYabE/raw"))()]]
+        Game = "UGC Paid Item ID",
+        Code = [[loadstring(game:HttpGet("https://pastefy.app/0R9lYabE/raw"))()]]
+    },
+    {
+        Game = "God Mode (Universal)",
+        Code = [[loadstring(game:HttpGet("https://pastebin.com/raw/DiJHNHmg"))()]]
     },
 }
