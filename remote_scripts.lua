c0924fe8468f5ca1de6ec8625ff83d676fc78746
HafiyScriptZ <ugchunter123@gmail.com>
2026-08-01 18:17:15 +0800

    Update script URL for 'Cheese Escape dark'

--- remote_scripts.lua
+++ remote_scripts.lua
@@ -13,6 +13,6 @@ return {
     },
     {
         Game = "Cheese Escape dark",
-        Code = [[loadstring(game:HttpGet("https://pastefy.app/9gYRjyi4/raw"))()]]
+        Code = [[loadstring(game:HttpGet("https://pastefy.app/SjHeYQ49/raw))()]]
     },
 }
